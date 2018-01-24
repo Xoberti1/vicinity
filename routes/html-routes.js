9104d68d7d2b5a36c686c2c6d2e41a763657112a
KrisB <33073049+KrisBierma@users.noreply.github.com>
2018-01-23 21:14:11 -0600

    add html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,6 +4,8 @@
 
 // Dependencies
 // =============================================================
+var express = require('express');
+var app = express();
 var path = require("path");
 
 // Routes
@@ -12,14 +14,28 @@ module.exports = function (app) {
 
     // Each of the below routes just handles the HTML page that the user gets sent to.
 
-    // index route loads view.html
+    // index route loads login.html
     app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public ..."));
+        res.sendFile(path.join(__dirname, "../public/login.html"));
     });
 
-    // authors route loads author-manager.html
-    app.get("/users", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public ..."));
+    // authors route loads home.html
+    app.get("/home", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
+    // authors route loads profile.html
+    app.get("/profile", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/profile.html"));
+    });
+
+    // authors route loads crimeform.html
+    app.get("/crimeform", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/crimeform.html"));
+    });
+
+    // authors route loads firstresponderform.html
+    app.get("/firstresponderform", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/firstresponderform.html"));
+    });
 };
