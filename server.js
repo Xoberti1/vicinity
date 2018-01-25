9454531e8c84c0fa082a832dfb53ddca2316ae0c
KrisB <33073049+KrisBierma@users.noreply.github.com>
2018-01-25 13:07:15 -0600

    app.use in server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
+app.use(express.static('public'));
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
