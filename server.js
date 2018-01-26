4969d07faecad843224838e147ebb9b0ff786e8a
Xoberti1 <xoberti@gmail.com>
2018-01-25 20:49:14 -0600

    routes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 
 // Sets up the Express App
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8085;
 
 // Requiring our models for syncing
 var db = require("./models");
@@ -34,4 +34,36 @@ db.sequelize.sync({}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
+//   sequelize.User.create(
+//     {
+//       firstName: "xavier",
+//       lastname: "oberti",
+//       username: "xoberti@gmail.com",
+//       password: "catinthehat",
+//       city: "harris county",
+//       state: "TX",
+//       Address: "11526 Moonmist Drive",
+//       zipCode: 77072,
+//     }
+//   )
 });
+
+
+// app.listen(PORT, () => {
+//   console.log("Your server has started.")
+//   db.sequelize.sync({}).then(() => {
+//     console.log("Your database is online.");
+//     db.User.create(
+//       {
+//         firstName: "xavier",
+//         lastName: "oberti",
+//         username: "xoberti@gmail.com",
+//         password: "catinthehat",
+//         city: "harris county",
+//         state: "TX",
+//         Address: "11526 Moonmist Drive",
+//         zipCode: 77072,
+//       }
+//     )
+//   })
+// });
